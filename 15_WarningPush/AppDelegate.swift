16b468f7077540caf832931b9fca86cdea36e920
YSLee-Dev <ysleedev01@gmail.com>
2022-06-02 18:52:53 +0900

    FCM 알림기능 구현

--- 15_WarningPush/AppDelegate.swift
+++ 15_WarningPush/AppDelegate.swift
@@ -7,17 +7,38 @@
 
 import UIKit
 import Firebase
+import UserNotifications
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
         
+        // FCM 현재 등록 토큰 확인
+        Messaging.messaging().token{ token, error in
+            if let error = error {
+                print(error)
+            }else if let token = token {
+                print("현재 토큰: ",token)
+            }
+            
+        }
+        
+        let authOptions : UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions){_,error in
+            print(error)
+        }
+        application.registerForRemoteNotifications()
         return true
     }
 
@@ -38,3 +59,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate : UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .sound, .badge, .banner])
+    }
+}
+
+extension AppDelegate : MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let fcmToken = fcmToken else {
+            return
+        }
+        print("변경된 토큰: ",fcmToken)
+
+    }
+}
